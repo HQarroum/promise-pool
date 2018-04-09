bd6d4700471d0f77c6a66db4cf815dcdb3e4a437
Halim Qarroum <hqm.post@gmail.com>
2018-04-09 09:27:53 +0200

    Add enqueueMany

---
+++ examples/enqueue-many/index.js
@@ -0,0 +1,28 @@
+const Pool = require('../../index');
+
+/**
+ * A promise pool of 5 elements.
+ */
+const pool = new Pool(5);
+
+/**
+ * Generates a random value between `min` and `max`.
+ * @return a number between `min` and `max`.
+ * @param min the lower number value to generate.
+ * @param min the higher number value to generate.
+ */
+const random = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+/**
+ * Process execution of a promise.
+ * @param idx the current promise index.
+ */
+const promise = (idx) => () => new Promise((resolve) => {
+  setTimeout(() => {
+    console.log(`Promise ${idx} running ...`);
+    resolve(idx);
+  }, random(0, 1 * 1000));
+});
+
+// Enqueuing two promises sequentially.
+pool.enqueueMany([ promise(1), promise(2), promise(3) ]).then(console.log);
