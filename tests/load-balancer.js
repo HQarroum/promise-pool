9bf958748ede2dda57caa55ceeca1f71c08ba410
Halim Qarroum <hqm.post@gmail.com>
2018-04-16 10:09:37 +0200

    Add .resize() for the load balancer strategy

--- tests/load-balancer.js
+++ tests/load-balancer.js
@@ -16,7 +16,7 @@ const evaluateAsPromise = (f) => new Promise((resolve, reject) => {
  * Process execution of a promise.
  * @param idx the current promise index.
  */
-const promise = (idx) => () => new Promise((resolve) => setTimeout(() => resolve(idx), idx * 100));
+const promise = (idx) => () => new Promise((resolve) => setImmediate(() => resolve(idx)));
 
 describe('The load balancer strategy', function () {
 
@@ -48,6 +48,91 @@ describe('The load balancer strategy', function () {
     this.pool.all()
       .then((results) => evaluateAsPromise(() => JSON.stringify(results).should.equal(results_)))
       .then(() => evaluateAsPromise(() => JSON.stringify(array).should.equal(expected)))
+      .then(callback)
+      .catch(callback);
+  });
+
+  /**
+   * Checking whether the strategy is able to upsize the pool
+   * dynamically.
+   */
+  it('should be able to upsize the pool dynamically', function (callback) {
+    const beforeEach = [];
+    const afterEach  = [];
+    const expected   = [0, 14, 1, 3, 7, 8, 4, 9, 10, 2, 5, 11, 12, 6, 13, 0, 1, 4, 6, 13];
+
+    // Setting the timeout value to `20000` milliseconds.
+    this.timeout(20000);
+    // Registering lifecycle events.
+    this.pool.beforeEach((idx) => beforeEach.push(idx)).afterEach((idx) => afterEach.push(idx));
+    // Resizing the pool.
+    this.pool.resize(15);
+    // Spreading `20` promises execution across the pool.
+    for (let i = 0; i < 20; ++i) {
+      this.pool.schedule(promise(i));
+    }
+    // Waiting for all the promises to be executed and
+    // displaying the results of the promise executions.
+    this.pool.all()
+      .then(() => evaluateAsPromise(() => (this.pool.opts.size === 15 && this.pool.strategy.pool.array.length === 15).should.be.true()))
+      .then(() => evaluateAsPromise(() => (JSON.stringify(beforeEach) === JSON.stringify(expected)).should.be.true()))
+      .then(() => evaluateAsPromise(() => (JSON.stringify(afterEach) === JSON.stringify(expected)).should.be.true()))
+      .then(callback)
+      .catch(callback);
+  });
+
+  /**
+   * Checking whether the strategy is able to downsize the pool
+   * dynamically.
+   */
+  it('should be able to downsize the pool dynamically', function (callback) {
+    const beforeEach = [];
+    const afterEach  = [];
+    const expected   = [5, 4, 1, 3, 2, 5, 4, 1, 3, 2, 5, 4, 1, 3, 2, 5, 4, 1, 3, 2];
+
+    // Setting the timeout value to `20000` milliseconds.
+    this.timeout(20000);
+    // Registering lifecycle events.
+    this.pool.beforeEach((idx) => beforeEach.push(idx)).afterEach((idx) => afterEach.push(idx));
+    // Resizing the pool.
+    this.pool.resize(5);
+    // Spreading `20` promises execution across the pool.
+    for (let i = 0; i < 20; ++i) {
+      this.pool.schedule(promise(i));
+    }
+    // Waiting for all the promises to be executed and
+    // displaying the results of the promise executions.
+    this.pool.all()
+      .then(() => evaluateAsPromise(() => (this.pool.opts.size === 5 && this.pool.strategy.pool.array.length === 5).should.be.true()))
+      .then(() => evaluateAsPromise(() => (JSON.stringify(beforeEach) === JSON.stringify(expected)).should.be.true()))
+      .then(() => evaluateAsPromise(() => (JSON.stringify(afterEach) === JSON.stringify(expected)).should.be.true()))
+      .then(callback)
+      .catch(callback);
+  });
+
+  /**
+   * Checking whether the strategy is able to resize the pool
+   * while scheduling new promises.
+   */
+  it('should be able to resize the pool while scheduling new promises', function (callback) {
+    const beforeEach = [];
+    const afterEach  = [];
+    const expected   = [0,9,1,3,7,8,4,2,5,6,0,9,1,7,5,6,0,7,5,0,7,5,0,7,5,0,7,5,0,7,5,0,7,5,0,7,5,0,7,5];
+
+    // Registering lifecycle events.
+    this.pool.beforeEach((idx) => beforeEach.push(idx)).afterEach((idx) => afterEach.push(idx));
+    // Spreading `40` promises execution across the pool.
+    for (let i = 0; i < 40; ++i) {
+      this.pool.schedule(promise(i));
+      if (i === 5) this.pool.resize(10);
+      if (i === 15) this.pool.resize(3);
+    }
+    // Waiting for all the promises to be executed and
+    // displaying the results of the promise executions.
+    this.pool.all()
+      .then(() => evaluateAsPromise(() => (this.pool.opts.size === 3 && this.pool.strategy.pool.array.length === 3).should.be.true()))
+      .then(() => evaluateAsPromise(() => (JSON.stringify(beforeEach) === JSON.stringify(expected)).should.be.true()))
+      .then(() => evaluateAsPromise(() => (JSON.stringify(afterEach) === JSON.stringify(expected)).should.be.true()))
       .then(callback)
       .catch(callback);
   });
